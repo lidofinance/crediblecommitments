693f3db0a1783971b87777c90caea3938d079739
KRogLA <i@krogla.me>
2025-01-14 19:06:23 +0100

    fix: interfaces upd

--- src/interfaces/IStakingModule.sol
+++ src/interfaces/IStakingModule.sol
@@ -1,7 +1,14 @@
 // SPDX-FileCopyrightText: 2025 Lido <info@lido.fi>
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity 0.8.24;
+pragma solidity 0.8.28;
+
+enum StakingModuleStatus {
+    Active, // deposits and rewards allowed
+    DepositsPaused, // deposits NOT allowed, rewards allowed
+    Stopped // deposits and rewards NOT allowed
+
+}
 
 interface IStakingModule {
     function getNodeOperatorsCount() external view returns (uint256);
