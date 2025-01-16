0e37753775a1d8e36b161c77e386838e10ee8300
KRogLA <i@krogla.me>
2025-01-16 11:06:24 +0100

    fix: interfaces upd

--- src/interfaces/ILidoLocator.sol
+++ src/interfaces/ILidoLocator.sol
@@ -4,9 +4,5 @@
 pragma solidity 0.8.28;
 
 interface ILidoLocator {
-    function burner() external view returns (address);
-    function coreComponents() external view returns (address, address, address, address, address, address);
-    function lido() external view returns (address);
     function stakingRouter() external view returns (address payable);
-    function treasury() external view returns (address);
 }
--- src/interfaces/IStakingModule.sol
+++ src/interfaces/IStakingModule.sol
@@ -11,6 +11,8 @@ enum StakingModuleStatus {
 }
 
 interface IStakingModule {
+    /// @notice Returns the type of the staking module
+    function getType() external view returns (bytes32);
     function getNodeOperatorsCount() external view returns (uint256);
     function getNodeOperatorIsActive(uint256 nodeOperatorId) external view returns (bool);
 }
