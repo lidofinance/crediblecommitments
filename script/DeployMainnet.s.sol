f1f7941ba6f51b64783b5e5b5778a76b04f8d7c5
KRogLA <i@krogla.me>
2025-01-21 11:05:00 +0100

    fix: undef mainnet constants

--- script/DeployMainnet.s.sol
+++ script/DeployMainnet.s.sol
@@ -10,15 +10,15 @@ contract DeployMainnet is DeployBase {
         // implementation constants
         config.lidoLocatorAddress = 0xC1d0b3DE6792Bf6b4b37EccdcC24e45978Cfd2Eb;
         config.csModuleType = "community-onchain-v1";
-        config.defaultOperatorMaxValidators = 100;
-        config.defaultBlockGasLimit = 1000000;
+        config.defaultOperatorMaxValidators = 1000;
+        config.defaultBlockGasLimit = 3000000;
 
         // proxy
-        config.proxyAdmin = 0x401FD888B5E41113B7c0C47725A742bbc3A083EF; // Dev team EOA
+        config.proxyAdmin = 0x0000000000000000000000000000000000000000; // Dev team EOA
 
         // initial parameters
-        config.committeeAddress = 0x401FD888B5E41113B7c0C47725A742bbc3A083EF; // Dev team EOA
-        config.optInMinDurationBlocks = 32;
+        config.committeeAddress = 0x0000000000000000000000000000000000000000; // Dev team EOA
+        config.optInMinDurationBlocks = 0;
         config.optOutDelayDurationBlocks = 64;
 
         _setUp();
